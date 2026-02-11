d7c5e08c5ae19077788b9b1b8f6ec9a582a54f7b
Nikica Radakovic <nikica.ra6@gmail.com>
2026-02-11 13:49:36 +0000

    operational margin calculation

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,7 @@
+select
+  om.orders_id,
+  om.date_date,
+  om.margin + s.shipping_fee - s.logCost - s.ship_cost as operational_margin
+from {{ ref('int_margin_orders') }} om
+left join {{ ref('stg_raw__ship') }} s
+using (orders_id)
