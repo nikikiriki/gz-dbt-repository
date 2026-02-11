658b8e982422afa9b3900d516c6dae33cc7b15d6
Nikica Radakovic <nikica.ra6@gmail.com>
2026-02-11 14:25:35 +0000

    fixing code

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,11 @@
 select
-  om.*,
   om.orders_id,
   om.date_date,
+  om.revenue,
+  om.purchase_cost,
+  s.shipping_fee,
+  s.logCost AS log_cost,
+  s.ship_cost,
   om.margin + s.shipping_fee - s.logCost - s.ship_cost as operational_margin
 from {{ ref('int_margin_orders') }} om
 left join {{ ref('stg_raw__ship') }} s
