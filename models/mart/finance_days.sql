d8c663f3b48cff8e95b42608bc6a392acb924925
Nikica Radakovic <nikica.ra6@gmail.com>
2026-02-11 15:04:37 +0000

    completed finance days table

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -6,7 +6,10 @@ with orders as
     revenue,
     purchase_cost,
     margin,
-    operational_margin
+    operational_margin,
+    shipping_fee,
+    log_cost,
+    ship_cost
     from {{ ref('int_orders_operational')}}
 ),
 qty_per_order as
@@ -26,6 +29,9 @@ select
   sum(o.margin) as total_margin,
   sum(o.operational_margin) as operational_margin,
   sum(o.purchase_cost) as total_purchase_cost,
+  sum(o.shipping_fee) as total_shipping_fees,
+  sum(o.log_cost) as total_log_costs,
+  sum(o.ship_cost) as total_ship_costs,
   sum(q.total_quantity) as total_quantity_sold
 from orders o
 left join qty_per_order q using (orders_id)
