34b206006fe8ca768fa962c5038cf039b36510c5
Nikica Radakovic <nikica.ra6@gmail.com>
2026-02-12 14:02:59 +0000

    intermediate view all campaigns

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,8 @@
+SELECT * FROM {{ ref('stg_raw_adwords') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_raw_bing') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_raw_criteo') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_raw_facebook') }}
+ 
